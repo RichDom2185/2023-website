13eb4b8702138069fd56a3af0f7ae16b992f4df3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-12 00:21:06 +0800

    Setup fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const tailwindConfig = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
+
+// Workaround due to https://github.com/microsoft/TypeScript/issues/47107
+module.exports = tailwindConfig;
