4c9245201d83a057a19429788f11ab429a6f8ce8
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-12 21:21:00 +0800

    Improve styling for ExternalLink
    * Adds dark theme style
    * Adds link text style

--- src/components/common/ExternalLink.tsx
+++ src/components/common/ExternalLink.tsx
@@ -11,13 +11,16 @@ const ExternalLink: ParentComponent<Props> = ({ to, hoverClass, children }) => {
       class="transition-colors duration-100"
       classList={{
         "hover:text-blue-600": !hoverClass,
+        "dark:hover:text-blue-300": !hoverClass,
         [hoverClass]: !!hoverClass,
       }}
       href={to}
       target="_blank"
       rel="noopener noreferrer"
     >
-      {children}
+      <span class="underline decoration-dotted hover:decoration-solid underline-offset-4">
+        {children}
+      </span>
     </a>
   );
 };
