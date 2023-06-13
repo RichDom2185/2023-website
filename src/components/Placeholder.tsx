cf991ae554c69e61e061ea5db5e05b84c8546690
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-13 22:20:53 +0800

    Allow custom children in Placeholder component
    This allows the component to be used as a container for non-functional
    children, e.g. components that are meant to be interactive.

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -1,11 +1,42 @@
-import { Component } from "solid-js";
+import classNames from "classnames";
+import { ParentComponent } from "solid-js";
+import { Classes } from "../utils/styles";
 
-const Placeholder: Component = () => {
+const Placeholder: ParentComponent = ({ children }) => {
   return (
-    <div>
-      <span class="block mx-5 px-3 py-16 text-center text-neutral-300 border-2 border-dashed select-none">
-        placeholder content
+    <div class="relative">
+      <span
+        class={classNames(
+          "block mx-5 px-3",
+          { "py-16": !children, "py-3": !!children },
+          "text-center text-neutral-300 border-2 border-dashed",
+          "select-none"
+        )}
+      >
+        {children ?? "placeholder content"}
       </span>
+      {children && (
+        <div
+          class={classNames(
+            "absolute top-1 right-1 bottom-1 left-1",
+            "mx-5",
+            "bg-white dark:bg-zinc-900",
+            "bg-opacity-50 dark:bg-opacity-60"
+          )}
+        >
+          <span
+            class={classNames(
+              "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+              Classes.TEXT_BOLD,
+              "font-display tracking-wider text-center text-4xl italic select-none",
+              "opacity-30 dark:opacity-60",
+              "-rotate-[24deg]"
+            )}
+          >
+            Coming soon!
+          </span>
+        </div>
+      )}
     </div>
   );
 };
