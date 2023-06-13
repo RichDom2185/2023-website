cdc634440d1326402eef2e4027f856202cb0202b
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-13 21:28:05 +0800

    Create placeholder component
    This will be used temporarily to fill missing sections of the site.

---
+++ src/components/Placeholder.tsx
@@ -0,0 +1,13 @@
+import { Component } from "solid-js";
+
+const Placeholder: Component = () => {
+  return (
+    <div>
+      <span class="block mx-5 px-3 py-16 text-center text-neutral-300 border-2 border-dashed select-none">
+        placeholder content
+      </span>
+    </div>
+  );
+};
+
+export default Placeholder;
