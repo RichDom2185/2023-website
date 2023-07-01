0dfdbe371ed0457534639ee539a3247fcd1bb1b3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-02 02:37:55 +0800

    Design ProjectDisplay component
    * Add conditional rendering of elements
    * Create initial design

--- src/components/data/ProjectDisplay.tsx
+++ src/components/data/ProjectDisplay.tsx
@@ -1,23 +1,44 @@
 import { Icon } from "@iconify-icon/solid";
+import classNames from "classnames";
 import { Component, For } from "solid-js";
 import { Project } from "../../types/data";
+import { Classes } from "../../utils/styles";
 
 // TODO: Investigate why optional props here are never typed as undefined
 const ProjectDisplay: Component<Project> = ({ name, description, links }) => {
   return (
-    <div>
-      <p>{name}</p>
-      <p>{description}</p>
-      <ul>
-        <For each={links}>
-          {(link) => (
-            <li>
-              <Icon inline icon={link.icon} />
-              <a href={link.url}>{link.label}</a>
-            </li>
-          )}
-        </For>
-      </ul>
+    <div class="space-y-1">
+      <h4 class={classNames(Classes.TEXT_MEDIUM, "text-xl")}>{name}</h4>
+      {description && <p>{description}</p>}
+      {links?.length && (
+        <ul class="!mt-3 space-y-1 text-sm">
+          <For each={links}>
+            {(link) => (
+              <li>
+                <a
+                  class={classNames(
+                    "hover:text-blue-600",
+                    "dark:hover:text-white"
+                  )}
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span
+                    class={classNames(
+                      "hover:underline underline-offset-[6px]",
+                      "space-x-2"
+                    )}
+                  >
+                    <Icon inline icon={link.icon} />
+                    <span>{link.label}</span>
+                  </span>
+                </a>
+              </li>
+            )}
+          </For>
+        </ul>
+      )}
     </div>
   );
 };
