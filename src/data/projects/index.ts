2fbf72e23e4627b7b3188da1fb6d9ca6662f1571
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-02 04:08:01 +0800

    Update test data
    * Add data for another year
    * Add some tech stacks to test data

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -1,11 +1,14 @@
 import { parse } from "yaml";
 import { Project } from "../../types/data";
-import projects_2022 from "./2022.yml?raw";
 
-export const projectYears = [2022] as const;
+import projects_2022 from "./2022.yml?raw";
+import projects_2023 from "./2023.yml?raw";
+
+export const projectYears = [2022, 2023] as const;
 
 export const allProjects: {
   [key in (typeof projectYears)[number]]: Project[];
 } = {
   2022: parse(projects_2022),
+  2023: parse(projects_2023),
 };
