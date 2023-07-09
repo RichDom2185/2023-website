fa89deae967463bbe4416fbe42e899b488195d7c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-09 20:00:59 +0800

    Migrate company links from ExpSummary to ExpDisplay
    Prevents misclicks when trying to click into the summary to show the
    full description. It is also more appropriate to have the links to the
    companies, which are less relevant to the actual experience, out of the
    summary component.
    
    Also includes some improvements:
    
    * Removed unnecessary createMemo
    * Removed unnecessary span tag around company name

--- src/components/data/ExperienceDisplay.tsx
+++ src/components/data/ExperienceDisplay.tsx
@@ -7,13 +7,35 @@ import TechnologyIcon from "../common/TechnologyIcon";
 
 // TODO: Investigate why optional props here are never typed as undefined
 const ExperienceDisplay: Component<Experience> = ({
-  company,
+  company: companyName,
   position,
   from,
   to,
   description,
+  link,
   tech,
 }) => {
+  const company = !link ? (
+    companyName
+  ) : (
+    <a
+      class={classNames("hover:text-blue-600", "dark:hover:text-white")}
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <span
+        class={classNames(
+          // Based on Classes.TEXT_DYNAMIC_COLOR
+          "decoration-zinc-900/30 dark:decoration-stone-400/20",
+          "underline hover:decoration-transparent decoration-dotted underline-offset-4"
+        )}
+      >
+        {companyName}
+      </span>
+    </a>
+  );
+
   return (
     <div class="font-sans">
       <p class={classNames("text-sm", "text-stone-600")}>
--- src/components/data/ExperienceSummary.tsx
+++ src/components/data/ExperienceSummary.tsx
@@ -1,42 +1,18 @@
-import classNames from "classnames";
-import { Component, createMemo } from "solid-js";
+import { Component } from "solid-js";
 import { Experience } from "../../types/data";
 
 // TODO: Investigate why optional props here are never typed as undefined
 const ExperienceSummary: Component<Experience> = ({
-  company: companyName,
+  company,
   position,
   from,
   to,
   link,
 }) => {
-  const company = createMemo(() =>
-    !link ? (
-      companyName
-    ) : (
-      <a
-        class={classNames("hover:text-blue-600", "dark:hover:text-white")}
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <span
-          class={classNames(
-            // Based on Classes.TEXT_DYNAMIC_COLOR
-            "decoration-zinc-900/30 dark:decoration-stone-400/20",
-            "underline hover:decoration-transparent decoration-dotted underline-offset-4"
-          )}
-        >
-          <span>{companyName}</span>
-        </span>
-      </a>
-    )
-  );
-
   return (
     <div class="font-display space-y-1">
       <p class="text-xs text-stone-600">
-        {company()} | {from} - {to}
+        {company} | {from} - {to}
       </p>
       <h4 class="italic">{position}</h4>
     </div>
