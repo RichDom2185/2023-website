3a7701185c23b7cc9ba73a1bef7eb8378082e599
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-04-22 18:59:45 +0800

    Refactor and use lazy loading in ExperienceSection
    * Use lazy loading for experience data
    * Improve typing
    * Add loading fallback

--- src/pages/index/ExperienceSection.tsx
+++ src/pages/index/ExperienceSection.tsx
@@ -1,20 +1,31 @@
 import { Icon } from "@iconify-icon/solid";
 import classNames from "classnames";
-import { Component, For } from "solid-js";
+import { Component, For, Show, createSignal } from "solid-js";
 import Placeholder from "../../components/Placeholder";
 import Section from "../../components/ui/Section";
-import { educationExperiences, workExperiences } from "../../data/experience";
+import { Experience } from "../../types/data";
 import { Classes } from "../../utils/styles";
 import ExperienceList from "./ExperienceList";
 import ExperienceShowcase from "./ExperienceShowcase";
 
-// TODO: Add more categories
-const categories = [
-  ["Work", workExperiences],
-  ["Education", educationExperiences],
-] as const;
+const loadExperience = () => import("../../data/experience");
 
 const ExperienceSection: Component = () => {
+  const [workHistory, setWorkHistory] = createSignal<Experience[]>();
+  const [educationHistory, setEducationHistory] = createSignal<Experience[]>();
+
+  const categories: () => ReadonlyArray<
+    [string, Experience[] | undefined]
+  > = () => [
+    ["Work", workHistory()],
+    ["Education", educationHistory()],
+  ];
+
+  loadExperience().then((module) => {
+    setWorkHistory(module.workExperiences);
+    setEducationHistory(module.educationExperiences);
+  });
+
   return (
     <>
       <Section>
@@ -43,23 +54,35 @@ const ExperienceSection: Component = () => {
       </Section>
       <Section>
         <div class={classNames(Classes.SECTION_BODY_RESPONSIVE, "space-y-20")}>
-          <For each={categories}>
+          <For each={categories()}>
             {([categoryName, categoryItems]) => (
-              <>
+              <Show
+                when={categoryItems}
+                fallback={
+                  <h2
+                    class={classNames(
+                      "text-2xl text-center font-display tracking-wider",
+                      Classes.TEXT_MEDIUM
+                    )}
+                  >
+                    Loading {categoryName}&hellip;
+                  </h2>
+                }
+              >
                 <div class="lg:hidden">
                   <ExperienceList
                     category={categoryName}
-                    items={categoryItems}
+                    items={categoryItems!}
                   />
                 </div>
                 <div class="hidden lg:block">
                   <ExperienceShowcase
                     category={categoryName}
-                    items={categoryItems}
+                    items={categoryItems!}
                     initialIndex={0}
                   />
                 </div>
-              </>
+              </Show>
             )}
           </For>
           <Placeholder disableOverlay>Other content coming soon!</Placeholder>
