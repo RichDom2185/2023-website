19acf71a97ebe03b2274738e4eda246e02a3fa7a
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-06-09 16:18:09 +0800

    Add 2021 projects

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -1,14 +1,16 @@
 import { Project } from "../../types/data";
 
+import projects_2021 from "./2021.yml";
 import projects_2022 from "./2022.yml";
 import projects_2023 from "./2023.yml";
 import projects_2024 from "./2024.yml";
 
-export const projectYears = [2022, 2023, 2024] as const;
+export const projectYears = [2021, 2022, 2023, 2024] as const;
 
 export const allProjects: {
   [key in (typeof projectYears)[number]]: Project[];
 } = {
+  2021: projects_2021,
   2022: projects_2022,
   2023: projects_2023,
   2024: projects_2024,
